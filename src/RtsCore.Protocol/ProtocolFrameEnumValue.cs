4ca03712cdaffe108849f8c685d0554b89ac5d89
tokihk <Toki.H.K@gmail.com>
2018-08-05 10:18:24 +0900

    プロトコルモジュール更新

--- src/RtsCore.Protocol/ProtocolFrameEnumValue.cs
+++ src/RtsCore.Protocol/ProtocolFrameEnumValue.cs
@@ -7,13 +7,17 @@ namespace RtsCore.Protocol
     public class ProtocolFrameEnumValue<EnumT> : ProtocolFrameValue
         where EnumT : struct
     {
-        private EnumT value_;
+        private UInt32 value_;
 
 
         public ProtocolFrameEnumValue(uint bitlen)
             : base(bitlen)
         {
-            value_ = (EnumT)Enum.GetValues(typeof(EnumT)).GetValue(0);
+            /* Enum型の最も小さい値を取る */
+            var value_e = (EnumT)Enum.GetValues(typeof(EnumT)).GetValue(0);
+
+            /* 整数型で記憶 */
+            value_ = Convert.ToUInt32(value_e);
         }
 
         public ProtocolFrameEnumValue(uint bitlen, EnumT value)
@@ -33,14 +37,14 @@ namespace RtsCore.Protocol
             get { return ((EnumT[])Enum.GetValues(typeof(EnumT))); }
         }
 
-        public EnumT Value
+        public UInt32 Value
         {
             get { return (value_); }
         }
 
         protected override void OnLoadBitData(ProtocolBitData bitdata)
         {
-            bitdata.SetInteger(0, BitLength, (ulong)(int)(object)value_);
+            bitdata.SetInteger(0, BitLength, value_);
         }
 
         protected override object OnLoadValue()
@@ -51,18 +55,21 @@ namespace RtsCore.Protocol
         protected override void OnSaveValue(object value)
         {
             try {
-                if (value.GetType() == typeof(EnumT)) {
-                    value_ = (EnumT)value;
-                } else {
-                    value_ = (EnumT)Enum.ToObject(typeof(EnumT), value);
-                }
+                value_ = Convert.ToUInt32(value);
             } catch {
             }
         }
 
         public override string ToString()
         {
-            return (value_.ToString());
+            /* Enum型に一致するものがあればEnum型で表示する */
+            try {
+                var value_e = Enum.ToObject(typeof(EnumT), value_);
+
+                return (string.Format("{0} ({1})", value_e.ToString(), value_));
+            } catch {
+                return (string.Format("{0}", value_));
+            }
         }
     }
 }
