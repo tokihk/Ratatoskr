731bd0d286a32c1816fd26c0da8a159e7025a4ed
tokihk <Toki.H.K@gmail.com>
2019-12-07 10:24:26 +0900

    pcapファイルの読み込みをマルチタスクで実行するように変更。 pcapファイルの読み込み状況をプログレスバーで確認できるようにした。

--- src/RtsPlugin.Pcap/FileFormats/PacketLog_Pcap/FileFormatReaderImpl.cs
+++ src/RtsPlugin.Pcap/FileFormats/PacketLog_Pcap/FileFormatReaderImpl.cs
@@ -13,13 +13,35 @@ namespace RtsPlugin.Pcap.FileFormats.PacketLog_Pcap
 {
     internal sealed class FileFormatReaderImpl : PacketLogReader
     {
+        private const int READ_PACKET_BLOCK_NUMBER = 200;
+
+
         private FileFormatOptionImpl      option_ = null;
         private PcapPacketParserOption    option_parser_ = null;
         private CaptureFileReaderDevice   device_ = null;
 
+        private Queue<SharpPcap.RawCapture>        packets_busy_in_ = new Queue<SharpPcap.RawCapture>();
+        private Queue<SharpPcap.RawCapture>        packets_busy_out_ = null;
+        private Queue<Queue<SharpPcap.RawCapture>> packets_list_ = new Queue<Queue<SharpPcap.RawCapture>>();
+
+        private ulong packets_count_in_  = 0;
+        private ulong packets_count_out_ = 0;
+
+        private IAsyncResult ar_load_device_packet_ = null;
+
 
         public FileFormatReaderImpl(FileFormatClass fmtc) : base(fmtc)
         {
+        }
+
+        public override ulong ProgressMax
+        {
+            get { return (packets_count_in_); }
+        }
+
+        public override ulong ProgressNow
+        {
+            get { return (packets_count_out_); }
         }
 
         protected override bool OnOpenPath(FileFormatOption option, string path)
@@ -34,14 +56,73 @@ namespace RtsPlugin.Pcap.FileFormats.PacketLog_Pcap
 
             device_.Filter = option_.Filter;
 
-            device_.Open();
+            ar_load_device_packet_ = (new LoadDevicePacketTaskDelegate(LoadDevicePacketTask)).BeginInvoke(device_, null, null);
 
             return (true);
         }
 
         protected override PacketObject OnReadPacket()
         {
-            return (SharpPcapPacketParser.Convert(device_, device_.GetNextPacket(), option_parser_));
+            var packet = (PacketObject)null;
+
+            do {
+                if (   (packets_busy_out_ == null)
+                    && (packets_list_.Count > 0)
+                ) {
+                    lock (packets_list_) {
+                        packets_busy_out_ = packets_list_.Dequeue();
+                    }
+                }
+
+                if (packets_busy_out_ != null) {
+                    packet = SharpPcapPacketParser.Convert(device_, packets_busy_out_.Dequeue(), option_parser_);
+                    if (packets_busy_out_.Count == 0) {
+                        packets_busy_out_ = null;
+                        packets_count_out_++;
+                    }
+                }
+            } while (
+                   (packet == null)
+                && (   (!ar_load_device_packet_.IsCompleted)
+                    || (packets_busy_out_ != null)
+                ));
+
+            return (packet);
+        }
+
+        private delegate void LoadDevicePacketTaskDelegate(CaptureFileReaderDevice device);
+        private void LoadDevicePacketTask(CaptureFileReaderDevice device)
+        {
+            device.OnPacketArrival += Device_OnPacketArrival;
+
+            device.Open();
+
+            /* 全パケットの読み込み開始(完了するまで戻ってこない) */
+            device_.Capture();
+
+            /* バッファにデータがあれば残りも登録 */
+            if (packets_busy_in_.Count > 0) {
+                lock (packets_list_) {
+                    packets_list_.Enqueue(packets_busy_in_);
+                }
+                packets_count_in_++;
+            }
+        }
+
+        private void Device_OnPacketArrival(object sender, SharpPcap.CaptureEventArgs e)
+        {
+            if (e.Packet != null) {
+                packets_busy_in_.Enqueue(e.Packet);
+
+                /* バッファがいっぱいになったらリストに登録してバッファをリロード */
+                if (packets_busy_in_.Count >= READ_PACKET_BLOCK_NUMBER) {
+                    lock (packets_list_) {
+                        packets_list_.Enqueue(packets_busy_in_);
+                    }
+                    packets_count_in_++;
+                    packets_busy_in_ = new Queue<SharpPcap.RawCapture>();
+                }
+            }
         }
     }
 }
